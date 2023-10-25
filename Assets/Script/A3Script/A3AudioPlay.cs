24f434eaf7c010d37ea9e0713b6aeab906219923
ZK <ZK@135.com>
2023-10-26 01:12:23 +0800

    Add sound resources, sound control code

---
+++ Assets/Script/A3Script/A3AudioPlay.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A3AudioPlay : MonoBehaviour
+{
+    public AudioClip StartAU, NomalAu;
+    private AudioSource audio;
+    private bool iSPlayStartAU = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.clip = StartAU;
+        audio.Play();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!audio.isPlaying&&iSPlayStartAU==false)
+        {
+            audio.clip = NomalAu;
+            audio.Play();
+            iSPlayStartAU = true;
+            audio.loop = true;
+        }
+    }
+}
