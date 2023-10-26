553cf1bc28bf74032a69efc9dbb91f83d1dd43c8
ZK <ZK@135.com>
2023-10-27 01:56:42 +0800

    Complete A4_50%

---
+++ Assets/Script/A4Script/StartPanel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartPanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Level1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
